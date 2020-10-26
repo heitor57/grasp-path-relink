d63261d5b6ed6f271eebbea64451e2d417a55866
heitor57 <heitorwerneck@hotmail.com>
2020-10-26 01:58:54 -0300

    file name, execution time

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,16 @@
 #include "Grasp.h"
 #include "objective_function.h"
 #include <ostream>
-
-std::ostream& operator<<(std::ostream &stream, const Item &a) {
-  return stream << "[u=" << a.utility << ",w="<<a.weight<<std::endl;
-}
+#include <ctime>
 
 int main(int argc, char **argv){
+  std::string data_dir = "../data/";
+  std::string results_dir =  data_dir+"results/";
+
   if(argc == 1){
     return 1;
   }
+
   std::ifstream in_file (argv[1]);
   std::string line;
   int num_items,capacity;
@@ -28,17 +29,22 @@ int main(int argc, char **argv){
     binary_knapsack.items[i].profit = binary_knapsack.items[i].utility/binary_knapsack.items[i].weight;
   }
   Grasp grasp(0.7,50,10,true);
+
+  clock_t begin = clock();
   auto solution = grasp.run(binary_knapsack);
+  clock_t end = clock();
+  double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
   std::cout << "Best solution: "
             << objective_function(binary_knapsack,solution)
-            << std::endl ;
-
-  
-  // std::default_random_engine generator(rd());
-  // std::cout << std::uniform_int_distribution<int>(0,9)(generator)
-  //           <<" "<<time(0)
-  //           << std::endl ;
-
+            << std::endl;
+  std::cout << "Elapsed time: " << elapsed_secs << " seconds\n";
+  std::string output_file_name = std::to_string(binary_knapsack.items.size())
+    +"-"+ std::to_string(binary_knapsack.capacity)
+    +"-"+ std::to_string(grasp.alpha)
+    +"-"+ std::to_string(grasp.iter_max)
+    +"-"+ std::to_string(grasp.num_elite)
+    +"-"+ std::to_string(grasp.use_path_relinking)
+    + ".txt";
   return 0;
 }
